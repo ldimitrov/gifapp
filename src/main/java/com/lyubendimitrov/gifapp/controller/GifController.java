5db2d579864091b37f831c8fde3a45de8cae6357
Lyuben Dimitrov <lyuben.dimitrov.ext@pentasys.de>
2016-09-10 13:53:36 +0200

    dinamic gif name with request

--- src/main/java/com/lyubendimitrov/gifapp/controller/GifController.java
+++ src/main/java/com/lyubendimitrov/gifapp/controller/GifController.java
@@ -1,11 +1,15 @@
 package com.lyubendimitrov.gifapp.controller;
 
+import com.lyubendimitrov.gifapp.data.GifRepository;
 import com.lyubendimitrov.gifapp.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 /**
@@ -14,15 +18,19 @@ import java.util.Date;
  */
 @Controller
 public class GifController {
+
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String listGifs() {
         return "index";
     }
 
-    @RequestMapping(value = "/gif", method = RequestMethod.GET)
-    public String gifDetails(ModelMap modelMap) {
+    @RequestMapping(value = "/gif/{name}", method = RequestMethod.GET)
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
         Date date = new Date();
-        Gif gif = new Gif("compiler-bot", date, "Lyuben", true);
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
--- src/main/java/com/lyubendimitrov/gifapp/model/Gif.java
+++ src/main/java/com/lyubendimitrov/gifapp/model/Gif.java
@@ -1,28 +1,53 @@
 package com.lyubendimitrov.gifapp.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Date;
+import java.time.LocalDate;
 
 
 /**
  * @author <a href="mailto:lyuben.dimitrov@comsysto.com">dimitrov</a>
  * @since 08.09.2016
  */
-@Getter
-@Setter
-@EqualsAndHashCode
 public class Gif {
     public String name;
-    private Date dateUploaded;
+    private LocalDate dateUploaded;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateUploaded() {
+        return dateUploaded;
+    }
+
+    public void setDateUploaded(LocalDate dateUploaded) {
+        this.dateUploaded = dateUploaded;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isFavourite() {
+        return favourite;
+    }
+
+    public void setFavourite(boolean favourite) {
+        this.favourite = favourite;
+    }
+
     private String username;
     private boolean favourite;
 
-    public Gif(String name, Date dateUploaded, String username, boolean favourite) {
+    public Gif(String name, LocalDate dateUploaded, String username, boolean favourite) {
         this.name = name;
         this.dateUploaded = dateUploaded;
         this.username = username;
