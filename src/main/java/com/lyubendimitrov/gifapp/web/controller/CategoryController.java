558a5361248c193c3253e32c49896b9278c3ee31
Lyuben Dimitrov <lyuben.dimitrov.ext@pentasys.de>
2017-05-01 11:12:01 +0200

    hibernate criteria for fetching categories

--- src/main/java/com/lyubendimitrov/gifapp/web/controller/CategoryController.java
+++ src/main/java/com/lyubendimitrov/gifapp/web/controller/CategoryController.java
@@ -1,23 +1,37 @@
 package com.lyubendimitrov.gifapp.web.controller;
 
 import com.lyubendimitrov.gifapp.model.Category;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
 @Controller
 public class CategoryController {
 
+    @Autowired
+    private SessionFactory sessionFactory;
+
     // Index of all categories
     @RequestMapping("/categories")
     public String listCategories(Model model) {
-        // TODO: Get all categories
-        List<Category> categories = new ArrayList<>();
+        Session session = sessionFactory.openSession();
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        CriteriaQuery<Category> criteria = builder.createQuery(Category.class);
+        criteria.from(Category.class);
+
+        // Execute Query
+        List<Category> categories = session.createQuery(criteria).getResultList();
 
         model.addAttribute("categories",categories);
         return "category/index";
