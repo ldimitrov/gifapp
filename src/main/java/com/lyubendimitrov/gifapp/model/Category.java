559a5890f43e32c81d1394b2cbcdb729ba57e41a
Lyuben Dimitrov <lyuben.dimitrov.ext@pentasys.de>
2017-05-01 19:20:42 +0200

    Added bean validation and custom flash messages

--- src/main/java/com/lyubendimitrov/gifapp/model/Category.java
+++ src/main/java/com/lyubendimitrov/gifapp/model/Category.java
@@ -7,6 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +19,13 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min = 3)
     private String name;
+
+    @NotNull
+    @Pattern(regexp = "#[0-9a-fA-F]{6}")
     private String colorCode;
 
     @OneToMany(mappedBy = "category")
--- src/main/java/com/lyubendimitrov/gifapp/web/controller/CategoryController.java
+++ src/main/java/com/lyubendimitrov/gifapp/web/controller/CategoryController.java
@@ -6,10 +6,13 @@ import com.lyubendimitrov.gifapp.web.Color;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -39,7 +42,9 @@ public class CategoryController {
     // Form for adding a new category
     @RequestMapping("categories/add")
     public String formNewCategory(Model model) {
-        model.addAttribute("category", new Category());
+        if (!model.containsAttribute("category")) {
+            model.addAttribute("category", new Category());
+        }
         model.addAttribute("colors", Color.values());
 
         return "category/form";
@@ -64,7 +69,13 @@ public class CategoryController {
 
     // Add a category
     @RequestMapping(value = "/categories", method = RequestMethod.POST)
-    public String addCategory(Category category) {
+    public String addCategory(@Valid Category category, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.category", result);
+            redirectAttributes.addFlashAttribute("category", category);
+            // Redirect back to the form
+            return "redirect:/categories/add";
+        }
         categoryService.save(category);
 
         return "redirect:/categories";
